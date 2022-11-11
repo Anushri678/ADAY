6215b1b5412548b0e34e995bdc76589eba3563c0
Anushri678 <110605839+Anushri678@users.noreply.github.com>
2022-11-11 11:33:27 +0530

    PracticalTest_10_Nov_2022

--- HibernateOneToOneMappingTest/src/main/java/com/project/hotom/App.java
+++ HibernateOneToOneMappingTest/src/main/java/com/project/hotom/App.java
@@ -6,26 +6,36 @@ import com.project.hotom.daoimpl.PersonDAOImpl;
 
 public class App 
 {
+	static  Scanner sc = new Scanner(System.in);
+	public static void subMenu() {
+		PersonDAOImpl personDAOImpl = new PersonDAOImpl();
+		System.out.println("1. Add new passport details");
+	       System.out.println("2. Display passport details");
+	       System.out.println("Enter your choice");
+	       int choice = sc.nextInt();
+	       switch(choice) {
+	       case 1: personDAOImpl.addPerson();
+	       break;
+	       case 2: personDAOImpl.displayPerson();
+	       break;
+	       default : System.out.println("Invalid Choice");
+	       }
+	}
     public static void main( String[] args )
     {
-    	
-       Scanner sc = new Scanner(System.in);
-       PersonDAOImpl personDAOImpl = new PersonDAOImpl();
        System.out.println("Passport Details");
-       System.out.println("1. Add new passport details");
-       System.out.println("2. Display passport details");
+       System.out.println("1. Start System");
+       System.out.println("2. End System");
        System.out.println("Enter your choice");
        int choice = sc.nextInt();
        do {
        switch(choice) {
-       case 1: personDAOImpl.addPerson();
+       case 1 : subMenu();
        break;
-       case 2: personDAOImpl.displayPerson();
+       case 2 : System.exit(0);
        break;
        default : System.out.println("Invalid Choice");
        }
        }while(true);
-       
-       
-}
+       }
 }
